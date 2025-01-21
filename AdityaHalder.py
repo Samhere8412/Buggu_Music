dbc004c9a53f54f1b13cc658b4aef34e9da5567b
Sameer14300 <116759845+Sameer14300@users.noreply.github.com>
2025-01-21 09:37:51 +0530

    Update AdityaHalder.py

--- AdityaHalder.py
+++ AdityaHalder.py
@@ -78,14 +78,14 @@ LOGGER = logging.getLogger("SYSTEM")
 if os.path.exists("Config.env"):
     load_dotenv("Config.env")
 
-API_ID = int(getenv("API_ID", 0))
-API_HASH = getenv("API_HASH", None)
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-STRING_SESSION = getenv("STRING_SESSION", None)
-MONGO_DB_URL = getenv("MONGO_DB_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", 0))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
-START_IMAGE_URL = getenv("START_IMAGE_URL", None)
+API_ID = int(getenv("API_ID", "23242170"))
+API_HASH = getenv("API_HASH", "87a55477c908fca3f9d8a83a4dca7799")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+STRING_SESSION = getenv("STRING_SESSION", "")
+MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://KingOfHell:Highspeedorg@cluster0.ha2cc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = int(getenv("OWNER_ID", "2073178010"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002314049781"))
+START_IMAGE_URL = getenv("START_IMAGE_URL", "https://graph.org/file/307e9ae4672c7b09071de-d136b56f1438c3a225.jpg")
 
 
 # Memory Database
@@ -1361,7 +1361,7 @@ With Your ☛ Other Friends.**"""
             [
                 InlineKeyboardButton(
                     text="🌺 Open Repository Link 🦋",
-                    url="https://github.com/AdityaHalder/AdityaPlayer",
+                    url="https://github.com/Sameer14300/Dudu_Music",
                 )
             ],
             [
